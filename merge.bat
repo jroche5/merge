65c3ce0d9191fe8b7980482f87d19c35738c7903
Jonathan Roche <jroche5@stumail.jccc.edu>
2017-02-01 13:17:45 -0600

    Merge

---
+++ merge.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Unity\Editor\Data\Tools\UnityYAMLMerge.exe" merge -p Blank.unity Red.unity Blue.unity End.unity
+"C:\Program Files\Unity\Editor\Unity.exe" -quit -batchmode -projectPath "C:\Users\svc-student\Desktop\UnityExample" -buildWindowsPlayer "C:\Users\svc-student\Desktop\UnityExample\build.exe"
